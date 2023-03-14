b554a41fa2575b965558445f601d1e12cc11cbcc
Parth12011999 <parthshinde30@gmail.com>
2023-03-14 14:41:29 +0530

    first commit

--- Bootstrap Template/template.js
+++ Bootstrap Template/template.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function fade() {
     var fadeIn = document.querySelectorAll(".fade-in");
 
@@ -15,4 +16,23 @@ function fade() {
     }
 }
 
+=======
+function fade() {
+    var fadeIn = document.querySelectorAll(".fade-in");
+
+    for(var i=0; i < fadeIn.length; i++)
+    {
+        var windowHeight = window.innerHeight;
+        var elementTop = fadeIn[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight -elementVisible) {
+           fadeIn[i].classList.add("active");
+        } else {
+            fadeIn[i].classList.remove("active");
+        }
+    }
+}
+
+>>>>>>> 640f6b2 (first commit)
 window.addEventListener("scroll", fade);
