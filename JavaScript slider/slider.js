bce89109d166083421cf12338a4e5b1c5272cd9e
Parth12011999 <parthshinde30@gmail.com>
2023-06-14 14:11:02 +0530

    done

--- JavaScript slider/slider.js
+++ JavaScript slider/slider.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const slides = document.querySelector(".img-show");
 var slideIndex = 0;
 const sliderImg = document.querySelector('.active-img')
@@ -96,3 +97,86 @@ Images.forEach(img=>{
 
 let some = Source.src.indexOf('assets/art5.jpg')
 console.log(some)
+=======
+const slides = document.querySelector(".img-show");
+var slideIndex = 0;
+const sliderImg = document.querySelector('.active-img')
+const Source = {
+    src : [
+    "https://picsum.photos/id/1/200/100",
+    "https://picsum.photos/id/2/200/100",
+    "https://picsum.photos/id/3/200/100",
+    "https://picsum.photos/id/4/200/100",
+    "https://picsum.photos/id/5/200/100",
+    "https://picsum.photos/id/6/200/100",
+    "https://picsum.photos/id/7/200/100",
+    "https://picsum.photos/id/8/200/100",
+    "https://picsum.photos/id/9/200/100",
+    "https://picsum.photos/id/10/200/100",
+    "https://picsum.photos/id/11/200/100",
+    "https://picsum.photos/id/12/200/100"
+    ]
+}
+
+Source.src.map(src=>{
+
+    // Creating Img tag using create elemnent
+    let ImgTag = document.createElement("img");
+    ImgTag.src = src;
+    console.log(ImgTag)
+    slides.appendChild(ImgTag)
+})
+let Images = document.querySelectorAll('.img-show img')
+console.log(Images)
+
+function blur(){
+    Images.forEach(img=>{
+        img.style.opacity = 0.3
+    })
+}
+blur()
+
+let nextButton = document.querySelector('.next');
+let prevButton = document.querySelector('.prev');
+
+// By default showing image
+Images[slideIndex].style.opacity = 1;
+sliderImg.style.backgroundImage = `url(${Images[slideIndex].src})`
+
+// Next button
+nextButton.addEventListener('click', ()=>{
+    slideIndex++
+    if (slideIndex > Images.length - 1) {
+        slideIndex = 0
+    }
+    // let ImgTag2 = sliderImg.createElement('img')
+    // ImgTag2.src = Images.src
+
+    blur()
+    Images[slideIndex].style.opacity = 1;
+    // let slide = Images[slideIndex]
+    sliderImg.style.backgroundImage = `url(${Images[slideIndex].src})`
+})
+
+// Prev button
+prevButton.addEventListener('click', ()=>{
+    slideIndex --
+    if (slideIndex < 0) {
+        slideIndex = Images.length - 1 
+    }
+    blur()
+    Images[slideIndex].style.opacity = 1;
+    // let slide = Images[slideIndex]
+    sliderImg.style.backgroundImage = `url(${Images[slideIndex].src})`
+
+})
+
+Images.forEach(img=>{
+    img.addEventListener('click',()=>{
+        blur()
+        img.style.opacity = 1;
+        sliderImg.style.backgroundImage = `url(${img.src})`
+    })
+})
+
+>>>>>>> 642df2e (done)
